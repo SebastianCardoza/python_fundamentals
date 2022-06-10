c2a45bcd33cf78080a3e9c075399843bcaf5337f
Sebastian Cardoza <jscboulangger@gmail.com>
2022-06-09 20:41:58 -0500

    math

--- math_dojo.py
+++ math_dojo.py
@@ -15,6 +15,9 @@ class MathDojo:
         return self
 
 md = MathDojo()
-
+md.add(2,3).add(3,5).add(1,2)
+print(md.result)
+md.subtract(2,3).subtract(3,5).subtract(1,2)
+print(md.result)
 x = md.add(2).add(2,5,1).subtract(3,2).result
 print(x)
